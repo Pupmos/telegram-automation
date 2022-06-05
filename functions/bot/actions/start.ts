1c1b1ae613ad422202e73783072f91d8f775f503
siriustaikun <sirius@ultrawebmail.xyz>
2022-06-05 21:11:44 +0000

    added translator

--- functions/bot/actions/start.ts
+++ functions/bot/actions/start.ts
@@ -3,17 +3,20 @@ import { TelegrafContext } from "telegraf/typings/context"
 
 import { getUser } from '../components/helper'
 import { memcache } from "../components/memcache"
+import { translate } from "../components/translate"
 
 export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafContext>) => {
   const { id, isBot, name } = getUser(ctx.from)
 
+  const msg = memcache.get(ctx.message.reply_to_message.message_id)
   if (isBot) {
     return ctx.reply(`Sorry I only interact with humans!`)
   }
-
-
+  if (!msg) {
+    return ctx.reply(`I don't remember seeing that message!`)
+  }
   try {
-    return ctx.reply(ctx.message.text + (ctx.callbackQuery?.message?.caption || '') + ctx.inlineMessageId + '' + JSON.stringify(memcache.get(ctx.message.reply_to_message.message_id)))
+    return ctx.reply(await translate(msg.text))
   } catch (e) {
     return ctx.reply(`Error occured`)
   }
