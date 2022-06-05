26507362607e4d585da6efc61f14927b71dd5ef9
siriustaikun <sirius@ultrawebmail.xyz>
2022-06-05 22:52:13 +0000

    moved training data to remote

--- functions/bot/components/translate.ts
+++ functions/bot/components/translate.ts
@@ -1,18 +1,20 @@
+import fetch from "cross-fetch";
 import { Configuration, OpenAIApi } from "openai";
-import * as fs from "fs";
-import * as path from "path";
-import { training } from "./training";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
+
+const loadTrainingSample = async () => {
+    return fetch('https://gist.githubusercontent.com/siriustaikun/95b2324aa0d55b7bdf0a44a1bfb7a028/raw/248bd738f4ceab28a4e90ef89f6c7af689fcea0f/openai-training-samples.txt').then(r => r.text())
+}
 
 export const translate = async function handler(text: string) {
   try {
 
     const sampleText = `dog: "${text}"\nhuman: `;
     const response = await openai.createCompletion("text-davinci-002", {
-      prompt: `${training}${sampleText}`,
+      prompt: `${await loadTrainingSample()}${sampleText}`,
       temperature: 0,
       max_tokens: 147,
       top_p: 1,
