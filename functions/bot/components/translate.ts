c08a4f18127ed4896055dbb68d297c9350d60253
siriustaikun <sirius@ultrawebmail.xyz>
2022-06-05 22:55:44 +0000

    added error logging

--- functions/bot/components/translate.ts
+++ functions/bot/components/translate.ts
@@ -23,7 +23,22 @@ export const translate = async function handler(text: string) {
     });
     return JSON.parse(response.data.choices[0].text.replace("\n\n", ""));
   } catch (e) {
-    console.error(e);
+    if (e.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(e.response.data);
+        console.log(e.response.status);
+        console.log(e.response.headers);
+      } else if (e.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(e.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', e.message);
+      }
+      console.log(e.config);
     return 'failed to translate'
   }
 }
