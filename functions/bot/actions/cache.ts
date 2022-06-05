b97f774921e01a8ce3f894a414b9f3c084816346
siriustaikun <sirius@ultrawebmail.xyz>
2022-06-05 21:16:54 +0000

    debug user

--- functions/bot/actions/cache.ts
+++ functions/bot/actions/cache.ts
@@ -6,6 +6,7 @@ import { memcache } from "../components/memcache"
 
 const cacheUserId = 5312444028
 export const cacheAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafContext>) => {
-  if (!ctx.message || ctx.from?.id !== cacheUserId) return;
+  console.log({ from: ctx.from?.id, cacheUserId})
+  if (!ctx.message || ctx.from.id !== cacheUserId) return;
   memcache.set(ctx.message.message_id, ctx.message!)
 }
