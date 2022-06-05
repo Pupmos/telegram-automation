3cb802ad2e03e8315bd0570ffa4b97b129479e66
siriustaikun <sirius@ultrawebmail.xyz>
2022-06-05 22:38:51 +0000

    turned training text to script file

---
+++ functions/bot/components/training.ts
@@ -0,0 +1,33 @@
+export const training = `Translate from dog language to human language in first person
+
+dog: "i am a berrie goob boi hoo lovbz hiz frenz berrie mach¡"
+human: "I am a very good boy who loves his friends very much!"
+
+dog: "i eet sausag fram flore"
+human: "I eat sausage from the floor."
+
+dog: "i licc licc ur ear"
+human: "I lick your ear."
+
+dog: "i reed da witepupper fram fromt 2 bacc"
+human: "I read the white paper from front to back."
+
+dog: "furthermoar cozmwazm enabolz contribushonz from ousside da core tendermindt team. thuz catalyzing the enfranchizement ov da communitea and prioritizing teh publicc prerogative wif rezpect 2 the protocolz evolushon ❤️"
+human: "Furthermore CosmWasm enables community contributions from outside the core Tendermint team, thus catalyzing the enfranchisement of the community and prioritizing the public prerogative with respect to the protocol's evolution."
+
+dog: "eye lovb 2 nuggol"
+human: "I love to snuggle."
+
+dog: "wifout a microgobernanze solushon lyke @DA0_DA0 teh cooperation overhed ov managing multisigz ultim8ly leadz 2 moar centrolized administrashon ov our cruciallie decentrolized servicez"
+human: "Without a microgovernance solution like @DAO_DAO, the cooperation overhead of managing multisigs ultimately leads to more centralized administration of our crucial decentralized services."
+
+dog: "i hav da best puptime on da networcc. i voat on everie puppozal. i giv da cozmoz happiez in timez ov gloomiez. i regularlie esspress mi valuez n princpupplez. i urge mi deleg8orz 2 deleg8 wif theirz."
+human: "I have the best uptime on the network. I vote on every proposal. I give the cosmos happiness in times of gloominess. I regularly express my values and principles. I urge my delegators to delegate with their own."
+
+dog: "don chooz a validator becaz ov moniez. dis iz importan. dis iz teh securitie ov teh networcc at steak. chooz a valid8or 4 der valuez, der frenship, der technical capupilitiez, der contributionz 2 de ecozystem, n teh unified goal ov decentralizashon. dat iz all. goob nite"
+human: "Do not choose a validator because of money. This is important. This is the security of the network at stake. Choose a validator for their values, their friendship, their technical capabilities, their contributions to the ecosystem, and the unified goal of decentralization. That is all. Good night."
+
+dog: "loocz lyke som1 needz a nuggol"
+human: "It looks like someone needs a snuggle!"
+
+`
