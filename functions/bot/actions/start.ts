dbbcf6791688cb718fa780e5f630d9cd4607cda7
siriustaikun <sirius@ultrawebmail.xyz>
2022-06-06 22:46:16 +0000

    added support for non-replies

--- functions/bot/actions/start.ts
+++ functions/bot/actions/start.ts
@@ -4,6 +4,7 @@ import { TelegrafContext } from "telegraf/typings/context"
 import { getUser } from '../components/helper'
 import * as gistcache from "../components/gistcache"
 import { translate } from "../components/translate"
+import { Message } from "telegraf/typings/telegram-types"
 
 export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafContext>) => {
   const { id, isBot, name } = getUser(ctx.from)
@@ -11,13 +12,18 @@ export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafCo
   if (isBot) {
     return ctx.reply(`Sorry I only interact with humans!`)
   }
-  const replyTargetId = ctx.message?.reply_to_message?.message_id;
-  if (!replyTargetId) {
-    return ctx.reply('message must be a reply!');
-  }
-  const msg = await gistcache.getItem(replyTargetId.toString())
-  if (!msg) {
-    return ctx.reply(`I don't remember seeing that message!`)
+  let msg: Message;
+  if (ctx.chat?.type !== 'private') {
+    const replyTargetId = ctx.message?.reply_to_message?.message_id;
+    if (!replyTargetId) {
+      return ctx.reply('message must be a reply!');
+    }
+    msg = await gistcache.getItem(replyTargetId.toString())
+    if (!msg) {
+      return ctx.reply(`I don't remember seeing that message!`)
+    }
+  } else {
+    msg = ctx.message
   }
   try {
     const formattedMessage = msg.text.split('\n').join(' ').split('\t').join(' ');
