5f03d5bd597c72de62db555159207f7f2a655b02
siriustaikun <siriustaikun@protonmail.com>
2022-11-29 21:45:29 +0000

    improve fallback response

--- functions/bot/actions/start.ts
+++ functions/bot/actions/start.ts
@@ -28,7 +28,7 @@ export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafCo
   // }
   try {
     const formattedMessage = msg.text.split('\n').join(' ').split('\t').join(' ');
-    const translatedText = await translate(formattedMessage).catch(() => translate(`What would you say if you didn't understand something that ${name} said?`));
+    const translatedText = await translate(formattedMessage).catch(() => translate(`${name} just said something incomprehenzibol. how do u rezpond?`));
     return ctx.replyWithMarkdownV2(`\`"${translatedText}"\``)
   } catch (e) {
     console.error(e);
