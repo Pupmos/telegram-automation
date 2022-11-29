b90eb222a43327c4b338ad12b599c7e3614f6553
siriustaikun <siriustaikun@protonmail.com>
2022-11-29 03:25:14 +0000

    removed public chat requirements

--- functions/bot/actions/start.ts
+++ functions/bot/actions/start.ts
@@ -13,18 +13,19 @@ export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafCo
     return ctx.reply(`Sorry I only interact with humans!`)
   }
   let msg: Message;
-  if (ctx.chat?.type !== 'private') {
-    const replyTargetId = ctx.message?.reply_to_message?.message_id;
-    if (!replyTargetId) {
-      return ctx.reply('message must be a reply!');
-    }
-    msg = await gistcache.getItem(replyTargetId.toString())
-    if (!msg) {
-      return ctx.reply(`I don't remember seeing that message!`)
-    }
-  } else {
-    msg = ctx.message
-  }
+  msg = ctx.message;
+  // if (ctx.chat?.type !== 'private') {
+  //   const replyTargetId = ctx.message?.reply_to_message?.message_id;
+  //   if (!replyTargetId) {
+  //     return ctx.reply('message must be a reply!');
+  //   }
+  //   msg = await gistcache.getItem(replyTargetId.toString())
+  //   if (!msg) {
+  //     return ctx.reply(`I don't remember seeing that message!`)
+  //   }
+  // } else {
+  //   msg = ctx.message
+  // }
   try {
     const formattedMessage = msg.text.split('\n').join(' ').split('\t').join(' ');
     const translatedText = await translate(formattedMessage);
