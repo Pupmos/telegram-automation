ac2a171b340173d61a378e68370d045e9a9cab0f
siriustaikun <siriustaikun@protonmail.com>
2022-11-30 08:27:44 +0000

    simplify allowance type

--- functions/bot/components/cosmwasm/feegrant.ts
+++ functions/bot/components/cosmwasm/feegrant.ts
@@ -20,27 +20,40 @@ export const grantFee = async (rpc: string, client: SigningStargateClient, denom
 
     //   if (!allowanceExists) {
         // Create feegrant allowance
+        // const allowance: Any = {
+        //   typeUrl: "/cosmos.feegrant.v1beta1.BasicAllowance",
+        //   value: Uint8Array.from(
+        //     PeriodicAllowance.encode({
+        //         basic: {
+        //             spendLimit: [
+        //                 {
+        //                     denom: denom,
+        //                     amount: spendLimit,
+        //                 },
+        //             ],
+        //         },
+        //         period: {
+        //             seconds: Long.fromNumber(14),
+        //             nanos: 0
+        //         },
+        //         periodSpendLimit: [],
+        //         periodCanSpend: []
+        //     }).finish(),
+        //   ),
+        // };
         const allowance: Any = {
-          typeUrl: "/cosmos.feegrant.v1beta1.BasicAllowance",
-          value: Uint8Array.from(
-            PeriodicAllowance.encode({
-                basic: {
-                    spendLimit: [
-                        {
-                            denom: denom,
-                            amount: spendLimit,
-                        },
-                    ],
-                },
-                period: {
-                    seconds: Long.fromNumber(14),
-                    nanos: 0
-                },
-                periodSpendLimit: [],
-                periodCanSpend: []
-            }).finish(),
-          ),
-        };
+            typeUrl: "/cosmos.feegrant.v1beta1.BasicAllowance",
+            value: Uint8Array.from(
+              BasicAllowance.encode({
+                spendLimit: [
+                  {
+                    denom: denom,
+                    amount: spendLimit,
+                  },
+                ],
+              }).finish(),
+            ),
+          };
         const grantMsg = {
           typeUrl: "/cosmos.feegrant.v1beta1.MsgGrantAllowance",
           value: MsgGrantAllowance.fromPartial({
