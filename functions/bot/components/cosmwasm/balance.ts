0dbe51f3062ff0e215de5b76c5fc922996a123e6
siriustaikun <siriustaikun@protonmail.com>
2022-11-30 07:02:48 +0000

    begin cw balance query

---
+++ functions/bot/components/cosmwasm/balance.ts
@@ -0,0 +1,17 @@
+import { createFeegrantAminoConverters } from "@cosmjs/stargate";
+import { connect } from "./connect";
+import { Cw20Client } from "./cw20";
+import { InstantiateMsg } from './Cw20.types'
+import { grantFee } from "./feegrant";
+import { junoConfig } from "./networks";
+
+
+export async function balance(contractAddress: string, fromUserId: number) {
+    let fromUserSigner = await connect(process.env.COSMOS_MNEMONIC!, junoConfig, fromUserId);
+    let cw20 = new Cw20Client(fromUserSigner.client, fromUserSigner.address, contractAddress);
+    let res = await cw20.balance({ address: fromUserSigner.address });
+    return {
+        chainId: junoConfig.chainId,
+        balance: +res.balance / 1_000_000,
+    }
+}
