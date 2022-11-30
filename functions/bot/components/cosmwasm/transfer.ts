85d32cf91da331290cec70d0973ce4b42f6fdbf6
siriustaikun <siriustaikun@protonmail.com>
2022-11-30 08:56:25 +0000

    rewarote cw exec as stargate

--- functions/bot/components/cosmwasm/transfer.ts
+++ functions/bot/components/cosmwasm/transfer.ts
@@ -4,7 +4,8 @@ import { Cw20Client } from "./cw20";
 import { InstantiateMsg } from './Cw20.types'
 import { grantFee } from "./feegrant";
 import { junoConfig } from "./networks";
-import { coins, StdFee } from "cosmwasm";
+import { coins, StdFee, toUtf8 } from "cosmwasm";
+import { MsgExecuteContract } from 'cosmjs-types/cosmwasm/wasm/v1/tx'
 
 export async function transferToken(contractAddress: string, amount: number, fromUserId: string, toUserId: string) {
     let rootSigner = await connect(process.env.COSMOS_MNEMONIC!, junoConfig);
@@ -12,14 +13,23 @@ export async function transferToken(contractAddress: string, amount: number, fro
     let toUserSigner = await connect(process.env.COSMOS_MNEMONIC!, junoConfig, toUserId);
 
     await grantFee(junoConfig.rpcEndpoint, rootSigner.stargateClient, junoConfig.feeToken, '100000', rootSigner.address, fromUserSigner.address)
-    fromUserSigner.stargateClient.sendTokens
-    let cw20 = new Cw20Client(fromUserSigner.client, fromUserSigner.address, contractAddress);
-    let res = await cw20.transfer({ amount: (amount * 1_000_000).toString(), recipient: toUserSigner.address }, {
+
+    const executeContractMsg = {
+        typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+        value: MsgExecuteContract.fromPartial({
+            sender: fromUserSigner.address,
+            contract: contractAddress,
+            msg: toUtf8(JSON.stringify({ amount: (amount * 1_000_000).toString(), recipient: toUserSigner.address })),
+            funds: [],
+        }),
+    };
+    const res = await fromUserSigner.stargateClient.signAndBroadcast(fromUserSigner.address, [executeContractMsg], {
         amount: coins('425', junoConfig.feeToken),
         gas: '169790',
         granter: rootSigner.address,
         payer: fromUserSigner.address
-    } as StdFee);
+    }, 'pupbot transfer');
+
     return {
         height: res.height,
         txHash: res.transactionHash,
