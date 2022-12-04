3fbb1c6d5e9575cd56254661f705892ddc1fda1c
Sirius Taikun <siriustaikun@protonmail.com>
2022-12-04 07:38:18 -0700

    load text during timeout

--- functions/bot/actions/start.ts
+++ functions/bot/actions/start.ts
@@ -30,8 +30,9 @@ export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafCo
     const formattedMessage = msg.text.split('\n').join(' ').split('\t').join(' ');
     let increaseInnocence = ctx.chat?.id === -1001261146654;
     // wait 5 seconds. slow down the bot until the open ai limit is increased
+    const translatedTextPromise = translate(formattedMessage, name, increaseInnocence);
     await new Promise(resolve => setTimeout(resolve, 5000));
-    const translatedText = await translate(formattedMessage, name, increaseInnocence);
+    const translatedText = await translatedTextPromise;
     // console.log(`\n\n\nchat_id:${ctx.chat?.id}\nprompt: "${formattedMessage}"\n\nresponse: "${translatedText}"\n\n`)
     return ctx.replyWithMarkdownV2(`\`"${translatedText}"\``, {
       reply_to_message_id: msg.message_id,
