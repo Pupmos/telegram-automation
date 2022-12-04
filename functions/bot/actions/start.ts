f68e7131394eefa9e0f7e8a74b16abb55774e312
Sirius Taikun <siriustaikun@protonmail.com>
2022-12-04 07:25:04 -0700

    added rate limiting

--- functions/bot/actions/start.ts
+++ functions/bot/actions/start.ts
@@ -29,9 +29,14 @@ export const startAction = async (ctx: TelegrafContext, bot: Telegraf<TelegrafCo
   try {
     const formattedMessage = msg.text.split('\n').join(' ').split('\t').join(' ');
     let increaseInnocence = ctx.chat?.id === -1001261146654;
+    // wait 5 seconds. slow down the bot until the open ai limit is increased
+    await new Promise(resolve => setTimeout(resolve, 5000));
     const translatedText = await translate(formattedMessage, name, increaseInnocence);
     // console.log(`\n\n\nchat_id:${ctx.chat?.id}\nprompt: "${formattedMessage}"\n\nresponse: "${translatedText}"\n\n`)
-    return ctx.replyWithMarkdownV2(`\`"${translatedText}"\``)
+    return ctx.replyWithMarkdownV2(`\`"${translatedText}"\``, {
+      reply_to_message_id: msg.message_id,
+      allow_sending_without_reply: true
+    })
   } catch (e) {
     console.error(e);
     return ctx.reply(`Error occured`)
