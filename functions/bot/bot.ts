99155269b82b85bf96be9efd59c3a79a606fd993
Sirius Taikun <siriustaikun@protonmail.com>
2022-12-22 12:02:55 -0700

    decrease random responses

--- functions/bot/bot.ts
+++ functions/bot/bot.ts
@@ -34,7 +34,7 @@ bot.on("text", async (ctx, next) => {
     // random message 10% of the time
     !ctx.message?.text.startsWith("/") &&
     isRespondable &&
-    Math.random() < 0.25
+    Math.random() < 0.001
   ) {
     return startAction(ctx, bot);
   }
