32a614cbaab6f097fc2dd12ecbf7657273d4a5bb
siriustaikun <siriustaikun@protonmail.com>
2023-02-08 08:14:35 +0000

    reduce frequency pentalty

--- functions/howl/components/queryGPT.ts
+++ functions/howl/components/queryGPT.ts
@@ -47,9 +47,9 @@ export const queryGPT = async function handler(
     const response = await openai.createCompletion("text-davinci-002", {
       prompt: `${await loadTrainingSample()}${sampleText}`,
       temperature: 0,
-      max_tokens: 147,
+      max_tokens: 70,
       top_p: 1,
-      frequency_penalty: 0.5,
+      frequency_penalty: 0,
       presence_penalty: 0,
     });
     // certain markdown characters break telegram https://stackoverflow.com/a/71313944
