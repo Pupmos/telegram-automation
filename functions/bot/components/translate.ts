faf3fc8c5539469cedfe7a224c8c190e08230771
SIRIUS | PUPMØS <99716325+siriustaikun@users.noreply.github.com>
2023-12-21 22:40:39 -0700

    Update translate.ts

--- functions/bot/components/translate.ts
+++ functions/bot/components/translate.ts
@@ -46,11 +46,12 @@ export const translate = async function handler(
         : `human (named ${name}): "${formattedText}"\ndog${dogModifier}:`;
     const response = await openai.createCompletion("gpt-3.5-turbo-instruct", {
       prompt: `${await loadTrainingSample()}${sampleText}`,
-      temperature: 0,
-      max_tokens: 147,
-      top_p: 1,
-      frequency_penalty: 0.5,
+      temperature: 1.01,
+      max_tokens: 200,
+      top_p: 0.84,
+      frequency_penalty: 0,
       presence_penalty: 0,
+      stop: ["\\n\\n"],
     });
     // certain markdown characters break telegram https://stackoverflow.com/a/71313944
     return JSON.parse(response.data.choices[0].text.replace("\n\n", ""))
